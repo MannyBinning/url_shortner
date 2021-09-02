a918c414d0b9b350c7cd4110e65aa7bc71f90fcc
MannyBinning <manib58@live.co.uk>
2021-09-02 21:49:30 +0000

    Created Random letter generator

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import (
     Flask, flash, render_template, 
     redirect, request, url_for)
 from flask_pymongo import PyMongo
+import random
+import string
 
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
@@ -19,9 +20,25 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+# Form Submission method
 @app.route("/index", methods=["GET","POST"])
 def index():
     return render_template("index.html")
+
+
+# Random letter generator method
+def shorten_url():
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        rand_letters = random.choices(letters, k=5)
+        rand_letters = "".join(rand_letters)
+        short_url = mongo.db.urls.find_one(
+            {"short_code": request.form.get("short_code")})
+        if not short_url:
+            return rand_letters
+        
+
+
 
 
 if __name__ == "__main__":
